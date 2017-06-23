d594431b81f7ae2cdcb07305d44bc061a5da23c6
Jeetan Madaan <home@Jeetans-MacBook-Pro.local>
2017-06-22 23:35:02 -0400

    second commit

--- dateconvertor/src/main/java/utils/DateConvertor.java
+++ dateconvertor/src/main/java/utils/DateConvertor.java
@@ -1,16 +1,28 @@
 package utils;
 
+
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateConvertor {
 	
-	public void convertDate() {
-		Date myDate = new Date();
-		System.out.println(myDate);
-		System.out.println(new SimpleDateFormat("MM-dd-yyyy").format(myDate));
-		System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(myDate));
-		System.out.println(myDate);
+	//public void convertDate(String convertDate, String fromFormat, String toFormat) throws Exception{
+		public String convertDate(String convertDate, String fromFormat , String toFormat) throws Exception{
+		System.out.println("InputParams: "+ convertDate + " "+ fromFormat + " "+ toFormat);
+		DateFormat format = new SimpleDateFormat(fromFormat, Locale.ENGLISH);
+		//Date myDate = new Date();
+		//Date fromDate = format.parse("02/19/2017");
+		Date fromDate = format.parse(convertDate);
+		System.out.println("FromDate: " + fromDate.toString());
+		((SimpleDateFormat) format).applyPattern(toFormat);
+		String toDate = format.format(fromDate);
+		//String toDate = new SimpleDateFormat(toFormat).format(fromDate);
+		System.out.println("ToDate: " + toDate);
+		System.out.println("InputParams: "+ convertDate + " "+ fromFormat + " "+ toFormat);
+		return toDate;
+		
 	}
 	 public String printHello() {
          
